92d880392a25c5252fba8b046518765973394290
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-09-11 01:48:05 +0300

    @DisplayName usage.

---
+++ src/test/java/com/muhammedtopgul/junit/aLevel/DisplayNameTest.java
@@ -0,0 +1,23 @@
+package com.muhammedtopgul.junit.aLevel;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author muhammed-topgul
+ * @since 11/09/2022 01:44
+ */
+@DisplayName("Customized display name tests.")
+public class DisplayNameTest {
+    @Test
+    @DisplayName("This is first test in this class.")
+    void test001() {
+        System.out.println("inside test001()");
+    }
+
+    @Test
+    @DisplayName("This is second test in this class.")
+    void test002() {
+        System.out.println("inside test002()");
+    }
+}
