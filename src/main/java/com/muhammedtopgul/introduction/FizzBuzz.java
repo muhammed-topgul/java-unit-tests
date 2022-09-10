c30a4938175fb7660b219d8568819711e3ae35f5
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-09-11 01:07:45 +0300

    FizzBuzz tests.

---
+++ src/main/java/com/muhammedtopgul/introduction/FizzBuzz.java
@@ -0,0 +1,27 @@
+package com.muhammedtopgul.introduction;
+
+/**
+ * @author muhammed-topgul
+ * @since 11/09/2022 00:50
+ */
+public class FizzBuzz {
+    public static String FIZZ = "Fizz";
+    public static String BUZZ = "Fizz";
+    public static String FIZZ_BUZZ = FIZZ + BUZZ;
+
+    public String stringFor(int number) {
+        if (number < 1 || number > 100) {
+            throw new IllegalArgumentException("Number must be between 1 and 100.");
+        }
+        if (number % 15 == 0) {
+            return FIZZ_BUZZ;
+        }
+        if (number % 3 == 0) {
+            return FIZZ;
+        }
+        if (number % 5 == 0) {
+            return BUZZ;
+        }
+        return String.valueOf(number);
+    }
+}
---
+++ src/test/java/com/muhammedtopgul/introduction/FizzBuzzTest.java
@@ -0,0 +1,51 @@
+package com.muhammedtopgul.introduction;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+/**
+ * @author muhammed-topgul
+ * @since 11/09/2022 00:50
+ */
+class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    void returnFizzWhenTheNumberIsDividedByThree() {
+        assertEquals(FizzBuzz.FIZZ, fizzBuzz.stringFor(3));
+    }
+
+    @Test
+    void returnFizzWhenTheNumberIsDividedByFive() {
+        assertEquals(FizzBuzz.BUZZ, fizzBuzz.stringFor(5));
+    }
+
+    @Test
+    void returnFizzBuzzWhenTheNumberIsDividedBothOfThreeAndFive() {
+        assertEquals(FizzBuzz.FIZZ_BUZZ, fizzBuzz.stringFor(15));
+    }
+
+    @Test
+    void returnTheNumberItselfWhenTheNumberIsNotDividedBothOfThreeAndFive() {
+        assertEquals("17", fizzBuzz.stringFor(17));
+    }
+
+    @Test
+    void throwsIllegalArgumentExceptionWhenTheNumberIsMoreThan100() {
+        assertThrows(IllegalArgumentException.class, () -> fizzBuzz.stringFor(105));
+    }
+
+    @Test
+    void throwsIllegalArgumentExceptionWhenTheNumberIsLessThen1() {
+        assertThrows(IllegalArgumentException.class, () -> fizzBuzz.stringFor(-1));
+    }
+}
