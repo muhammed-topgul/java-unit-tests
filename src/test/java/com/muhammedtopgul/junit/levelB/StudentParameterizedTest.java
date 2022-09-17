e2f51a0c74addeb0d444e1ca754badd4aaf58106
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-09-18 00:46:58 +0300

    Test Info and Test Reporter parameters.

---
+++ src/test/java/com/muhammedtopgul/junit/levelB/StudentParameterizedTest.java
@@ -0,0 +1,52 @@
+package com.muhammedtopgul.junit.levelB;
+
+import com.muhammedtopgul.model.Student;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author muhammed-topgul
+ * @since 18/09/2022 00:31
+ */
+@DisplayName("Level B (Intermediate) Student Test with TestInfo and TestReporter Parameters")
+public class StudentParameterizedTest {
+    private Student student;
+
+    public StudentParameterizedTest(TestInfo testInfo) {
+        System.out.println("Display Name: " + testInfo.getDisplayName());
+        System.out.println("Tags: " + testInfo.getTags());
+        System.out.println("Test Class: " + testInfo.getTestClass());
+        System.out.println("Test Method: " + testInfo.getTestMethod());
+    }
+
+    @BeforeEach
+    void setUp(TestInfo testInfo) {
+        if (testInfo.getTags().contains("create")) {
+            student = new Student("1", "Muhammed", "Topgul");
+        } else {
+            student = new Student("1", "John", "Doe");
+        }
+    }
+
+    @Test
+    @DisplayName("Create Student")
+    @Tag("create")
+    void createStudent(TestInfo testInfo) {
+        assertTrue(testInfo.getTags().contains("create"));
+        assertEquals("Muhammed", student.getName());
+    }
+
+    @Test
+    @DisplayName("Add Course to Student")
+    @Tag("addCourse")
+    void addCourseToStudent(TestReporter testReporter, TestInfo testInfo) {
+        assertTrue(testInfo.getTags().contains("addCourse"));
+        testReporter.publishEntry("Start Time: ", LocalDateTime.now().toString());
+        assertEquals("John", student.getName());
+        testReporter.publishEntry("End Time: ", LocalDateTime.now().toString());
+    }
+}
