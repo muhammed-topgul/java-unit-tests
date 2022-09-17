b4d7aed0c92306ec6daded47544b189fa2bbad42
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-09-18 01:42:58 +0300

    Parameterized tests with @EnumSource.

--- src/test/java/com/muhammedtopgul/junit/levelB/StudentWithParameterizedMethodTest.java
+++ src/test/java/com/muhammedtopgul/junit/levelB/StudentWithParameterizedMethodTest.java
@@ -1,5 +1,6 @@
 package com.muhammedtopgul.junit.levelB;
 
+import com.muhammedtopgul.enumeration.CourseType;
 import com.muhammedtopgul.model.Course;
 import com.muhammedtopgul.model.LecturerCourseRecord;
 import com.muhammedtopgul.model.Semester;
@@ -8,10 +9,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author muhammed-topgul
@@ -38,6 +41,47 @@ public class StudentWithParameterizedMethodTest {
             studentCourseSize++;
             assertEquals(studentCourseSize, student.getStudentCourseRecords().size());
             assertTrue(student.isTakeCourse(new Course(courseCode)));
+        }
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class EnumSourceTest {
+        @BeforeAll
+        void setUp() {
+            student = new Student("1", "Muhammed", "Topgul");
+        }
+
+        @ParameterizedTest
+        @EnumSource(CourseType.class)
+        void addCourseToStudent(CourseType courseType) {
+            Course course = Course.builder().code(String.valueOf(new Random().nextInt(200))).courseType(courseType).build();
+            LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(course, new Semester());
+            student.addCourse(lecturerCourseRecord);
+            assertFalse(student.getStudentCourseRecords().isEmpty());
+            assertTrue(student.isTakeCourse(course));
+        }
+
+        @ParameterizedTest
+        @EnumSource(value = CourseType.class, names = "MANDATORY")
+        void addMandatoryCourseToStudent(CourseType courseType) {
+            Course course = Course.builder().code(String.valueOf(new Random().nextInt(200))).courseType(courseType).build();
+            LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(course, new Semester());
+            student.addCourse(lecturerCourseRecord);
+            assertFalse(student.getStudentCourseRecords().isEmpty());
+            assertTrue(student.isTakeCourse(course));
+            assertEquals(CourseType.MANDATORY, course.getCourseType());
+        }
+
+        @ParameterizedTest
+        @EnumSource(value = CourseType.class, mode = EnumSource.Mode.EXCLUDE, names = "MANDATORY")
+        void addElectiveCourseToStudent(CourseType courseType) {
+            Course course = Course.builder().code(String.valueOf(new Random().nextInt(200))).courseType(courseType).build();
+            LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(course, new Semester());
+            student.addCourse(lecturerCourseRecord);
+            assertFalse(student.getStudentCourseRecords().isEmpty());
+            assertTrue(student.isTakeCourse(course));
+            assertEquals(CourseType.ELECTIVE, course.getCourseType());
         }
     }
 }
